d184a7a852da39b3bd944e99f956b6aef0a4f9bd
taras <promverstat@ukr.net>
2020-12-25 21:57:39 +0200

    fix

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,24 +1,12 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.support.GenericXmlApplicationContext;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.web.meal.MealRestController;
-import ru.javawebinar.topjava.web.user.AdminRestController;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
-
-import static ru.javawebinar.topjava.TestUtil.mockAuthorize;
-import static ru.javawebinar.topjava.UserTestData.user;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
+/*
         try (GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext()) {
             appCtx.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.REPOSITORY_IMPLEMENTATION);
             appCtx.load("spring/inmemory.xml");
@@ -40,5 +28,12 @@ public class SpringMain {
             System.out.println();
             System.out.println(mealController.getBetween(null, null, null, null));
         }
+*/
+
+        String unsafe =
+                "<p><a href='http://example.com/' onclick='stealCookies()'>Link</a></p>";
+        String safe = Jsoup.clean(unsafe, Whitelist.basic());
+        // now: <p><a href="http://example.com/" rel="nofollow">Link</a></p>
+        System.out.println(safe);
     }
 }
